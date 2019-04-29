823ab603c9300e075fc754140ae793e792b11352
hho114 <hho114@csu.fulleton.edu>
2019-04-29 00:03:50 -0700

    delete process.cpp

---
+++ process.h
@@ -0,0 +1,30 @@
+
+#pragma once
+#include <iostream>
+#include <queue>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
+
+class Process
+{
+public:
+
+  Process(){}
+
+private:
+
+  int id;
+  int mem_block_count;
+  int tot_memory;
+  int turn_around;
+  int start_at;
+  int end_at;
+  int life_time;
+  vector <int> memmory_block;
+
+
+};
